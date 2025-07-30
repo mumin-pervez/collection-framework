98ce1772ab4ebd8dd3b4d6ae6951c363af45d249
mumin-pervez <pervez.ist@gmail.com>
2025-07-30 19:27:31 +0600

    No arg constructor or default constructor of ArrayList

---
+++ src/list/arraylist/NoArgConstructor.java
@@ -0,0 +1,13 @@
+package list.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoArgConstructor {
+
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    System.out.println(list.isEmpty());
+  }
+
+}

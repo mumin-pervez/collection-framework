f8c791392941b96b68799a74b3e1f2f2666d9742
mumin-pervez <pervez.ist@gmail.com>
2025-08-03 11:20:53 +0600

    Added Core methods in Collection Interface

---
+++ src/collection/MethodsInCollectionInterface.java
@@ -0,0 +1,44 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MethodsInCollectionInterface {
+
+  public static void main(String[] args) {
+    Collection collection = new ArrayList();
+    collection.add("Hello");
+    collection.add(120);
+    collection.add(909);
+    collection.add(null);
+    collection.add(789.37);
+    collection.add(null);
+
+    //int size() - Returns the number of elements
+    int sizeOfCollection = collection.size();
+    System.out.println("Number of elements : " + sizeOfCollection);
+
+    //boolean isEmpty() - Returns true if empty
+    boolean isEmpty = collection.isEmpty();
+    System.out.println("Collection is empty ? : " + isEmpty);
+
+    //boolean contains(Object o) - Checks if element exists
+    boolean containsAnyElement = collection.contains(120);
+    System.out.println("Is this available? : " + containsAnyElement);
+
+    //boolean add(E e) - Adds an element (optional operation)
+    boolean addItem = collection.add("Moyna");
+    System.out.println("Add an item : " + addItem);
+    System.out.println(collection);
+
+    //boolean remove(Object o) - Removes an element (optional operation)
+    boolean removeAnItem = collection.remove(120);
+    System.out.println("The specific object is removed : " + removeAnItem);
+    System.out.println(collection);
+
+
+
+
+  }
+
+}

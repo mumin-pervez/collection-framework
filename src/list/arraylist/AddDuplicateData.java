ca916222fc4e89ef2378c44c153e238cb0915e94
mumin-pervez <pervez.ist@gmail.com>
2025-08-04 18:22:56 +0600

    Duplicate data can be added in ArrayList

---
+++ src/list/arraylist/AddDuplicateData.java
@@ -0,0 +1,16 @@
+package list.arraylist;
+
+import java.util.ArrayList;
+
+public class AddDuplicateData {
+
+  public static void main(String[] args) {
+    ArrayList<String> al = new ArrayList<>();
+    al.add("Apple");
+    al.add("Banana");
+    al.add("Apple");
+
+    System.out.println(" " + al);
+  }
+
+}

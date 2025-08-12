6305eec48b54c7265cba66686b0415dc721cbd11
mumin-pervez <pervez.ist@gmail.com>
2025-08-12 14:50:00 +0600

    Adding elements to Set

---
+++ src/set/SetDemo.java
@@ -0,0 +1,15 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo {
+
+  public static void main(String[] args) {
+    Set<String> set = new HashSet<>();
+    set.add("Mumin");
+    set.add("Anik");
+    set.add("Topu");
+    System.out.println(set);
+  }
+}

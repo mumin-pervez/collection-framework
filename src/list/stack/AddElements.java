2f0b33fd1ed05e2f58c1ed40eb625e3f68834369
mumin-pervez <pervez.ist@gmail.com>
2026-05-13 10:19:57 +0600

    Add elements in Stack

---
+++ src/list/stack/AddElements.java
@@ -0,0 +1,18 @@
+package list.stack;
+
+import java.util.Stack;
+
+public class AddElements {
+
+  public static void main(String[] args) {
+    Stack stack = new Stack();
+    stack.push("A");
+    stack.push("E");
+    stack.push("I");
+    stack.push("O");
+    stack.push("U");
+    System.out.println(stack); // [A, E, I, O, U]
+
+  }
+
+}

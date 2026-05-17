8a6f49b1d9110350492a3ec00e4324b853bca1a1
mumin-pervez <pervez.ist@gmail.com>
2026-05-17 19:30:07 +0600

    only one null insertion possible

---
+++ src/set/treeset/NullInsertion.java
@@ -0,0 +1,23 @@
+package set.treeset;
+
+import java.util.TreeSet;
+
+public class NullInsertion {
+
+  public static void main(String[] args) {
+    TreeSet treeSet = new TreeSet();
+    System.out.println(treeSet); // [ ]
+
+    treeSet.add("CSE");
+    treeSet.add("BBA");
+    treeSet.add("ECE");
+    System.out.println(treeSet); // [BBA, CSE, ECE]
+    treeSet.add(" "); // Only one null value will be inserted
+    treeSet.add("Apple");
+    System.out.println(treeSet); // [ , Apple, BBA, CSE, ECE]
+    treeSet.add(" "); // Only one null value will be inserted
+    treeSet.add("BBA"); // Duplicates will not be inserted
+    System.out.println(treeSet); // [ , Apple, BBA, CSE, ECE]
+  }
+
+}
